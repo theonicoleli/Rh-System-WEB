856948491c420cc732a188dccd54b0aa10f9cf37
Théo Nicoleli <118020861+theonicoleli@users.noreply.github.com>
2023-10-18 14:17:15 -0300

    RH WEB

---
+++ RhSystem/src/public/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Tilt+Neon&display=swap" rel="stylesheet">
+    <title>Pessoas Empresa</title>
+</head>
+<body>
+    <div class="header div">
+        <h1 class="title funcionarios">Funcionários</h1>
+        <h1 class="title addfuncionario">Adicionar funcionários</h1>
+    </div>
+    <div class="body div">
+        <h1>Aplicação com PHP e DATABASE</h1>
+    </div>
+</body>
+</html>
